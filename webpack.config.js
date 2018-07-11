bb6d05259d9d6db29f969cd225b0316ae206b8c0
KGrabowski <Kamil.Grabowski@hemmersbach.com>
2018-07-11 15:03:27 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports ={
+    devtool: 'inline-source-map',
+    entry: [
+        'webpack-dev-server/client?http://127.0.0.1:8080/',
+        'webpack/hot/only-dev-server',
+        './src'
+    ],
+};
