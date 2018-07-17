6f4877392781536aac3d46fd334954a48e0a9f42
KGrabowski <Kamil.Grabowski@hemmersbach.com>
2018-07-17 15:36:24 +0200

    create

--- src/components/create-toDo.js
+++ src/components/create-toDo.js
@@ -3,11 +3,19 @@ import React from 'react';
 export default class CreateToDo extends React.Component {
     render() {
         return (
-           <form>
-           <input type="text" placeholder="What do I need to do?"/>
+           <form onSubmit={this.handleCreate.bind(this)}>
+           <input type="text" placeholder="What do I need to do?" ref="createInput"/>
                <button>CREATE</button>
            </form>
 
         );
     }
+
+    handleCreate(event) {
+
+        event.preventDefault();
+
+        console.log(this.refs.createInput.value);
+    }
+
 }
