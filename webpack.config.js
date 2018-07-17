af1d228e5fc1544c596a4815d6cb9615774e752a
KGrabowski <Kamil.Grabowski@hemmersbach.com>
2018-07-17 08:08:12 +0200

    console.log(message) PASS

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,24 @@ let webpack = require('webpack');
 let path = require('path');
 
 module.exports = {
+    devServer: {
+        contentBase: path.resolve(__dirname, './'),
+        port: 3000,
+        stats: 'minimal',
+        disableHostCheck: true,
+        historyApiFallback: true,
+    },
     devtool: 'inline-source-map',
     entry: [
-        'webpack-dev-server/client?http://12777/',
+        'webpack-dev-server/client?http://127.0.0.1:3000/',
         'webpack/hot/only-dev-server',
         './src'
     ],
     output: {
         path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:3000/',
+        chunkFilename: '[id].chunk.js'
     },
     resolve: {
         modulesDirectories: ['node_modules', 'src'],
